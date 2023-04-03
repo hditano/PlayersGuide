0216273530256be9d42298209da4aeaa19e94d20
Hernan Di Tano <hditano@gmail.com>
2023-04-03 19:47:14 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConsoleApp2;
+
+var oMyName = new Book("Hernan", "Di Tano", 1983, "Accion", "El Quinto Elemento", new int[] {2,3,4,5});
+Console.WriteLine(oMyName.FullName("Sasa"));
+int [] myArray = {2, 3, 4, 5, 6};
+Book.Array(myArray);
+Console.ReadLine();
+
+namespace ConsoleApp2
+{
+    class Book
+    {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public int YearOfRelease { get; private set; }
+        public string Genre { get; private set; }
+        public string NameOfBook { get; private set; }
+        public int[] myArray { get; private set; }
+
+        public Book(string myname, string myLastName, int year, string genre, string nameBook, int[] array)
+        {
+            Name = myname;
+            LastName = myLastName;
+            YearOfRelease = year;
+            Genre = genre;
+            NameOfBook = nameBook;
+            myArray = array;
+        }
+
+        public string FullName(string result)
+        {
+            string my_book =
+                $"The Book is named {NameOfBook} of Genre: {Genre}, Released in {YearOfRelease}. Written by {Name} {LastName}";
+            return my_book;
+        }
+
+        public static void Array(int[] arr)
+        {
+            Console.WriteLine(arr);
+                for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+
+       
+
+    }
+}
+
+
---
+++ TheReplicatorOfToDo/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Reflection.PortableExecutable;
+
+int[] myArray = new int[5];
+
+Console.WriteLine("Please type in 5 values: ");
+Console.WriteLine(myArray.Length);
+
+for(int i = 0; i < myArray.Length; i++)
+{
+    Console.WriteLine("Please type a value: ");
+    string myTemp = Console.ReadLine();
+    myArray[i] = Int32.Parse(myTemp);
+}
+
+int[] secondArray = new int[5];
+
+for(int i = 0; i < secondArray.Length; i++)
+{
+    secondArray[i] = myArray[i];
+}
+
+for(int i = 0; i< secondArray.Length;i++)
+{
+    Console.WriteLine($"My first array has the following values: {myArray[i]}");
+    Console.WriteLine($"My second array has the following values: {secondArray[i]}");
+}
