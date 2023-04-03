6cb7f4fd37c025615bccc4b1ecc61b58a27d9025
Hernan Di Tano <hditano@gmail.com>
2023-04-03 19:56:22 +0200

    Array Exercise 2 - Done

---
+++ TheLawsOfFreach/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue; // Start higher than anything in the array.
+
+foreach(int item in array)
+{
+    if (item < currentSmallest)
+        currentSmallest = item;
+}
+
+Console.WriteLine(currentSmallest);
