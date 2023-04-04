32408f896d1156668e2bfd0d7c6a5a646654c2e1
Hernan Di Tano <hditano@gmail.com>
2023-04-04 04:00:19 +0200

    Methods Exercises - Done

---
+++ Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(AskForNumber("Whats your age: "));
+
+int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int result = Convert.ToInt16(Console.ReadLine());
+    return result;
+    
+}
+
+
+Console.WriteLine(AskForNumberInRange("Type a Number", 10, 30));
+
+int AskForNumberInRange(string text, int min, int max)
+{
+    int num;
+    do
+    {
+        Console.WriteLine(text);
+        num = Convert.ToInt32(Console.ReadLine());
+
+    } while (num < min || num > max);
+
+    return num;
+}
