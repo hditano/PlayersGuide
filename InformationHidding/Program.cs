eee71fa34da7b1c97f2b02d74eeeecdc28e74579
Hernan Di Tano <hditano@gmail.com>
2023-04-05 13:26:02 +0200

    Information Hidding Exercise - Done

---
+++ InformationHidding/Program.cs
@@ -0,0 +1,101 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Arrow myArrow = new Arrow(ArrowHead.steel, Fletching.plastic, 300);
+myArrow.GetCost();
+
+Console.WriteLine(myArrow.GetLength());
+
+class Arrow
+{
+    private ArrowHead arrowhead;
+    private Fletching fletching;
+    private float length;
+    private float cost;
+
+    public Arrow(ArrowHead arrowhead, Fletching fletching, float length)
+    {
+        this.arrowhead = arrowhead;
+        this.fletching = fletching;
+        this.length = length;
+    }
+
+    public ArrowHead GetArrowHead()
+    {
+        return this.arrowhead;
+    }
+
+    public Fletching GetFletching()
+    {
+        return this.fletching;
+    }
+
+    public float GetLength()
+    {
+        return this.length;
+    }
+
+    public void GetCost()
+    {
+        Console.WriteLine($"Cost of the following combination is: {this.arrowhead} {this.fletching} {this.length}");
+        CalculateCost();
+        Console.WriteLine($"Total cost is: {this.cost}");
+    }
+
+    public void CalculateCost()
+    {
+
+        switch (this.arrowhead)
+        {
+            case ArrowHead.wood:
+                this.cost += 3;
+                break;
+            case ArrowHead.steel:
+                this.cost += 10;
+                break;
+            case ArrowHead.obsidian:
+                this.cost += 5;
+                break;
+            default:
+                break;
+        }
+
+        switch (this.fletching)
+        {
+            case Fletching.turkey:
+                this.cost += 5;
+                break;
+            case Fletching.plastic:
+                this.cost += 5;
+                break;
+            case Fletching.goose:
+                this.cost += 3;
+                break;
+            default:
+                break;
+        }
+
+        Console.Write("Please Choose a Length: ");
+        float tmpLength = (float)Convert.ToDouble(Console.ReadLine());
+        this.length += (float)(tmpLength * 0.5);
+        this.cost += this.length;
+
+
+    }
+}
+
+
+
+enum ArrowHead
+{
+    steel = 10,
+    wood = 3,
+    obsidian = 5
+}
+
+enum Fletching
+{
+    plastic = 10,
+    turkey = 5,
+    goose = 3
+}
+
