c71d488b24064180962fdd0c2519d16c4e719180
Hernan Di Tano <hditano@gmail.com>
2023-04-07 00:03:29 +0200

    TheLockedDoor Exercise - Done

--- TheLockedDoor/Program.cs
+++ TheLockedDoor/Program.cs
@@ -1,8 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Door myDoor = new Door(State.open, 123);
-myDoor.newPassword(123, 3030);
-myDoor.newPassword(3030, 2020);
+myDoor.StartDoor(123);
 
 class Door
 {
@@ -26,7 +25,7 @@ class Door
         get => this._password;
         set => this._password = value;
     }
-    
+
     public State isUnlocked()
     {
         return this._status = State.locked;
@@ -49,7 +48,7 @@ class Door
 
     public void newPassword(int currentPassword, int password)
     {
-        if(password > 1 && currentPassword == this._password)
+        if (password > 1 && currentPassword == this._password)
         {
             this._password = password;
             Console.WriteLine($"New Password has been set {this._password}");
@@ -58,12 +57,37 @@ class Door
         {
             Console.WriteLine($"Current password es: {this._password}");
             Console.WriteLine("Current Password is Wrong");
-            
+
         }
 
     }
 
-    
+    public void StartDoor(int password)
+    {
+        if (password == this._password)
+        {
+            Console.WriteLine("Welcome to the Door Flow: ");
+
+            while (true)
+            {
+
+                Console.WriteLine($"Current state is: {this._status}");
+                Console.Write("What would like you to do?: ");
+                string reply = Console.ReadLine();
+
+                switch (reply)
+                {
+                    case "close": if (this._status == State.closed) this._status = State.open; break;
+                    case "open": if (this._status == State.open) this._status = State.closed; break;
+                    case "lock": if (this._status == State.locked) this._status = State.unlocked; break;
+                    case "unlock": if (this._status == State.unlocked) this._status = State.locked; break;
+                    case "default": break;
+                };
+            }
+        }
+
+
+    }
 }
 
 enum State
