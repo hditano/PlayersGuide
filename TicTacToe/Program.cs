932c07685411e3d0d6d8bec842e77f6771673ea9
Hernan Di Tano <hditano@gmail.com>
2023-04-08 18:01:01 +0200

    Changed _results array for char

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -24,18 +24,18 @@ class Player
 class Board
 {
     private int[] _board { get; set; } = new int[9];
-    private string[] _results { get; set; } = new string[9] {"A", "B", "C", "D", "E", "F", "G", "H", "I"};
+    private char[,] _results { get; set; } = new char[3, 3] { { 'X', 'X', 'X' }, { 'X', 'X', 'X'}, { 'X', 'X', 'X'} };
 
     public void ShowBoard()
     {
         Console.WriteLine("Tic Tac Toe");
         Console.WriteLine("-------------");
         Console.WriteLine("");
-        Console.WriteLine($"  {_results[0]} | {_results[1]} | {_results[2]} ");
+        Console.WriteLine($"  {_results[0,0]} | {_results[0,1]} | {_results[0,2]} ");
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[3]} | {_results[4]} | {_results[5]} "); 
+        Console.WriteLine($"  {_results[1,0]} | {_results[1,1]} | {_results[1,2]} "); 
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[6]} | {_results[7]} | {_results[8]} ");
+        Console.WriteLine($"  {_results[2,0]} | {_results[2,1]} | {_results[2,2]} ");
     }
 
 }
