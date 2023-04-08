90b0130042dfb28cab40489040c807f55cd4bcbc
Hernan Di Tano <hditano@gmail.com>
2023-04-08 02:43:10 +0200

    TicTacToe - First Commit

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Board newB = new Board();
+newB.ShowBoard();
+
+class TicTacToe
+{
+    
+}
+
+
+class Player
+{
+    private string? _name { get; set; }
+    private string? _symbol { get; set; }
+
+    internal Player(string name, string symbol)
+    {
+        _name = name;
+        _symbol = symbol;
+    }
+}
+
+class Board
+{
+    private int[] _board { get; set; } = new int[8];
+
+    public void ShowBoard()
+    {
+        foreach(var item in _board)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+}
