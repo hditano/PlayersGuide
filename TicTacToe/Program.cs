e07ddec7070e972cd39367f43a07d52a63bb4927
Hernan Di Tano <hditano@gmail.com>
2023-04-09 01:06:21 +0200

    Refactor - Printing value choosen via X,X will render perfectly - WIP

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -1,20 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Board newB = new Board();
-newB.ShowBoard();
+new TicTacToe().Run();
 
 class TicTacToe
 {
-    
+
+    private Player player1 { get; } = new Player("Hernan", 'X');
+    private Player player2 { get; } = new Player("Zoe", 'O');
+    private int counter { get; set; } = 0;
+
+    public void Run()
+    {
+        Board new1 = new Board();
+        Console.Write("Please choose a new square as [row,col] ");
+        string input = Console.ReadLine();
+        string[] tmp_array = input.Split(',');
+        int[] ints = tmp_array.Select(int.Parse).ToArray();
+        new1.CheckPosition(player1, ints);
+        new1.ShowBoard();
+
+    }
 }
 
 
 class Player
 {
-    private string? _name { get; set; }
-    private string? _symbol { get; set; }
+    internal string? _name { get; set; }
+    internal char _symbol { get; set; }
 
-    internal Player(string name, string symbol)
+    internal Player(string name, char symbol)
     {
         _name = name;
         _symbol = symbol;
@@ -24,18 +38,31 @@ class Player
 class Board
 {
     private int[] _board { get; set; } = new int[9];
-    private char[,] _results { get; set; } = new char[3, 3] { { 'X', 'X', 'X' }, { 'X', 'X', 'X'}, { 'X', 'X', 'X'} };
+    private char[,] _results { get; set; } = new char[3, 3];
 
     public void ShowBoard()
     {
         Console.WriteLine("Tic Tac Toe");
         Console.WriteLine("-------------");
         Console.WriteLine("");
-        Console.WriteLine($"  {_results[0,0]} | {_results[0,1]} | {_results[0,2]} ");
+        Console.WriteLine($"  {_results[0,0]}  | {_results[0,1]}  | {_results[0,2]} ");
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[1,0]} | {_results[1,1]} | {_results[1,2]} "); 
+        Console.WriteLine($"  {_results[1,0]}  | {_results[1,1]}  | {_results[1,2]} "); 
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[2,0]} | {_results[2,1]} | {_results[2,2]} ");
+        Console.WriteLine($"  {_results[2,0]}  | {_results[2,1]}  | {_results[2,2]} ");
+    }
+
+    public void CheckPosition(Player p, int[] array)
+    {
+        char isEmpty = '\0';
+        if (_results[0, 0] == isEmpty) _results[array[0], array[1]] = 'X';
+        else Console.WriteLine("Its not empty");
+    }
+
+    public void CreateSymbol(Player p)
+    {
+        Console.WriteLine($"Hello Player: {p._name}");
+        _results[0, 1] = p._symbol;
     }
 
 }
