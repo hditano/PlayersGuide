57bbc15e1dcea4dbaf529f6cedd0d432f8cd133d
Hernan Di Tano <hditano@gmail.com>
2023-04-09 15:04:46 +0200

    Refactor CheckPosition to loop while isDone condition is not true

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -12,11 +12,8 @@ class TicTacToe
     public void Run()
     {
         Board new1 = new Board();
-        Console.Write("Please choose a new square as [row,col] ");
-        string? input = Console.ReadLine();
-        string[] tmp_array = input.Split(',');
-        int[] ints = tmp_array.Select(int.Parse).ToArray();
-        new1.CheckPosition(player1, ints);
+        new1.CreateSymbol(player1);
+        new1.CheckPosition(player1);
         new1.ShowBoard();
 
     }
@@ -45,18 +42,37 @@ class Board
         Console.WriteLine("Tic Tac Toe");
         Console.WriteLine("-------------");
         Console.WriteLine("");
-        Console.WriteLine($"  {_results[0,0]}  | {_results[0,1]}  | {_results[0,2]} ");
+        Console.WriteLine($"  {_results[0, 0]}  | {_results[0, 1]}  | {_results[0, 2]} ");
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[1,0]}  | {_results[1,1]}  | {_results[1,2]} "); 
+        Console.WriteLine($"  {_results[1, 0]}  | {_results[1, 1]}  | {_results[1, 2]} ");
         Console.WriteLine($" --- --- ---");
-        Console.WriteLine($"  {_results[2,0]}  | {_results[2,1]}  | {_results[2,2]} ");
+        Console.WriteLine($"  {_results[2, 0]}  | {_results[2, 1]}  | {_results[2, 2]} ");
     }
 
-    public void CheckPosition(Player p, int[] array)
+    public void CheckPosition(Player p)
     {
         char isEmpty = '\0';
-        if (_results[array[0], array[1]] == isEmpty) _results[array[0], array[1]] = p._symbol;
-        else Console.WriteLine("Its not empty");
+        bool isDone = false;
+
+
+        while (!isDone)
+        {
+            Console.Write("Please choose a new square as [row,col] ");
+            string? input = Console.ReadLine();
+            string[] tmp_array = input.Split(',');
+            int[] ints = tmp_array.Select(int.Parse).ToArray();
+            isDone = false;
+
+            if (_results[ints[0], ints[1]] == isEmpty)
+            {
+                _results[ints[0], ints[1]] = p._symbol;
+                isDone = true;
+            }
+            else
+            {
+                Console.WriteLine("Square is already taken");
+            }
+        }
     }
 
     public void CreateSymbol(Player p)
@@ -64,5 +80,5 @@ class Board
         Console.WriteLine($"Hello Player: {p._name}");
         _results[0, 1] = p._symbol;
     }
-
 }
+
