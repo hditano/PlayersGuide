1c0b241316bbefb34c580c930c80bed9bc1c51e7
Hernan Di Tano <hditano@gmail.com>
2023-04-12 23:34:56 +0200

    Generics Exercise Done

---
+++ Generics/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+ColoredItem<Sword> coloredSword = new ColoredItem<Sword>(ConsoleColor.DarkBlue, new Sword());
+coloredSword.Display();
+
+public class Sword { }
+public class Bow { }
+public class Axe { }
+
+public class ColoredItem<T>
+{
+    ConsoleColor _CColor { get; set; }
+    T _Item { get; set; }
+
+    public ColoredItem(ConsoleColor ccolor, T item)
+    {
+        _CColor = ccolor;
+        _Item = item;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = _CColor;
+        Console.WriteLine(_Item);
+    }
+}
