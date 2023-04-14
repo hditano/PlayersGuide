d576015615a5b13ee980f4e4e444a04d8cc0ac9c
Hernan Di Tano <hditano@gmail.com>
2023-04-14 11:08:42 +0200

    working on checkmovement method

--- TheFountainOfObjects/Program.cs
+++ TheFountainOfObjects/Program.cs
@@ -8,20 +8,21 @@ public class Grid
     public int[,] _MyGrid;
     public int _X;
     public int _Y;
-    private int _index = 0;
+    public int _index;
 
     public Grid(int x, int y)
     {
         _X = x;
         _Y = y;
         _MyGrid = new int[_X, _Y];
+        _index = 0;
     }
 
     public void AddValuesToGrid()
     {
-        for(var i = 0; i < _MyGrid.GetLength(0); i++)
+        for (var i = 0; i < _MyGrid.GetLength(0); i++)
         {
-            for(var j = 0; j < _MyGrid.GetLength(1); j++)
+            for (var j = 0; j < _MyGrid.GetLength(1); j++)
             {
                 _MyGrid[i, j] = _index;
                 _index++;
@@ -31,9 +32,9 @@ public class Grid
 
     public void Display()
     {
-        for(var i = 0; i < _MyGrid.GetLength(0);i++)
+        for (var i = 0; i < _MyGrid.GetLength(0); i++)
         {
-            for(var j = 0; j < _MyGrid.GetLength(1);j++)
+            for (var j = 0; j < _MyGrid.GetLength(1); j++)
             {
                 int value = _MyGrid[i, j];
                 int length = value.ToString().Length;
@@ -50,11 +51,47 @@ public class Grid
 public class State : StateProps
 {
 
-    public static void CheckPosition()
+    private Grid _MyGrid;
+
+    public State(Grid MyGrid)
     {
-        State state = new State();
-        state._PlayerPosition = (0, 1);
+        _MyGrid = MyGrid;
     }
+
+    public void CheckPosition()
+    {
+
+    }
+
+    public void CheckMovement()
+    {
+        bool continueLoop = true;
+        for (var i = 0; i < _MyGrid._MyGrid.GetLength(0); i++)
+        {
+            for (var j = 0; j < _MyGrid._MyGrid.GetLength(1); j++)
+            {
+                while (continueLoop)
+                {
+                    if (_MyGrid._MyGrid[i, j] > 0 || _MyGrid._MyGrid[i, j] < 15)
+                    {
+                        Console.WriteLine("Please move right: ");
+                        string? reply = Console.ReadLine();
+
+                        switch (reply)
+                        {
+                            case "right": _PlayerPosition = (0, _PlayerPosition.Item1 + 1); continueLoop = false; break;
+                            case "default": break;
+
+                        }
+
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine($"Player Position now is: {_PlayerPosition.Item1} {_PlayerPosition.Item2}");
+    }
+
 }
 
 // abstract class that creates props
@@ -88,17 +125,18 @@ public class MainLoop
         bool GameOver = false;
         Grid MyGame = new Grid(4, 4);
         MyGame.AddValuesToGrid();
+        State state = new State(MyGame);
 
-        while(!GameOver)
+        while (!GameOver)
         {
             MyGame.Display();
-            State.CheckPosition();
+            state.CheckMovement();
 
             string? input = Console.ReadLine();
 
             Console.Clear();
         }
-        
+
 
     }
 
