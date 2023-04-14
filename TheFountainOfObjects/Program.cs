f5e1d5253dfe726083d54c32c8650792aa1ad12a
Hernan Di Tano <hditano@gmail.com>
2023-04-14 14:52:43 +0200

    Refactor FountainOfObjects

--- TheFountainOfObjects/Program.cs
+++ TheFountainOfObjects/Program.cs
@@ -1,143 +1,91 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-MainLoop.Start();
+MainLoop.Run();
 
-// The main grid class generates a grid based on the values of X and Y passed to it, and populates each array position with values ranging from 0 to XX for future reference.
-public class Grid
+public class MainLoop
 {
-    public int[,] _MyGrid;
-    public int _X;
-    public int _Y;
-    public int _index;
-
-    public Grid(int x, int y)
+    public static void Run()
     {
-        _X = x;
-        _Y = y;
-        _MyGrid = new int[_X, _Y];
-        _index = 0;
+        State state = new State();
+        state.CheckPlayerPosition();
+
     }
+}
 
-    public void AddValuesToGrid()
+
+public class Grid : StateProps
+{
+    private int[,] _MyGrid { get; set; }
+
+
+    public Grid()
     {
-        for (var i = 0; i < _MyGrid.GetLength(0); i++)
-        {
-            for (var j = 0; j < _MyGrid.GetLength(1); j++)
-            {
-                _MyGrid[i, j] = _index;
-                _index++;
-            }
-        }
+        _MyGrid = new int[4, 4];
     }
 
     public void Display()
     {
-        for (var i = 0; i < _MyGrid.GetLength(0); i++)
-        {
-            for (var j = 0; j < _MyGrid.GetLength(1); j++)
-            {
-                int value = _MyGrid[i, j];
-                int length = value.ToString().Length;
-                if (length == 1) Console.Write($" {value} ");
-                else Console.Write($"{value} ");
-            }
-            Console.WriteLine();
-        }
     }
 
 }
 
-// State managment class that inherits from Stateprops
 public class State : StateProps
 {
 
-    private Grid _MyGrid;
+    Grid MyGrid = new Grid();
 
-    public State(Grid MyGrid)
+    public State()
     {
-        _MyGrid = MyGrid;
     }
 
-    public void CheckPosition()
+    public void CheckPlayerPosition()
     {
+        Console.WriteLine("What do you want to do?: ");
+        string? reply = Console.ReadLine();
 
-    }
-
-    public void CheckMovement()
-    {
-        bool continueLoop = true;
-        for (var i = 0; i < _MyGrid._MyGrid.GetLength(0); i++)
+        switch(reply)
         {
-            for (var j = 0; j < _MyGrid._MyGrid.GetLength(1); j++)
-            {
-                while (continueLoop)
-                {
-                    if (_MyGrid._MyGrid[i, j] > 0 || _MyGrid._MyGrid[i, j] < 15)
-                    {
-                        Console.WriteLine("Please move right: ");
-                        string? reply = Console.ReadLine();
-
-                        switch (reply)
-                        {
-                            case "right": _PlayerPosition = (0, _PlayerPosition.Item1 + 1); continueLoop = false; break;
-                            case "default": break;
-
-                        }
-
-                    }
-                }
-            }
+            case "move north": 
+                var newLocation = new Location(_PlayerLocation.row, _PlayerLocation.col - 1);
+                if(IsOnMap(newLocation)) 
+                    _PlayerLocation = newLocation; break;
+            case "move south": 
+                var newLocation1 = new Location(_PlayerLocation.row, _PlayerLocation.col + 1);
+                if(IsOnMap(newLocation1)) 
+                    _PlayerLocation = newLocation1;  break;
+            case "move east":  
+                var newLocation2 = new Location(_PlayerLocation.row - 1, _PlayerLocation.col);
+                if (IsOnMap(newLocation2))
+                    _PlayerLocation = newLocation2;  break;
+            case "move west": 
+                var newLocation3 = new Location(_PlayerLocation.row + 1, _PlayerLocation.col);
+                if(IsOnMap(newLocation3))
+                    _PlayerLocation = newLocation3;  break;
+            default:break;
         }
 
-        Console.WriteLine($"Player Position now is: {_PlayerPosition.Item1} {_PlayerPosition.Item2}");
+        Console.WriteLine(_PlayerLocation);
     }
 
+    public bool IsOnMap(Location location) => location.row >= 0 && location.row <= 4 && location.col >= 0 && location.col <= 4; 
+    
 }
 
-// abstract class that creates props
-public abstract class StateProps
+
+public class StateProps
 {
-    protected bool _Fountain { get; set; }
-    protected (int, int) _FountainPosition { get; }
-    protected bool _EntranceExit { get; set; }
-    protected (int, int) _EntranceExitPosition { get; }
-    protected (int, int) _PlayerPosition { get; set; }
+    protected Location _FountainOfObjects { get; }
+    protected Location _PlayerLocation { get; set; }
 
     public StateProps()
     {
-        _Fountain = false;
-        _FountainPosition = (0, 2);
-        _EntranceExitPosition = (0, 0);
-        _PlayerPosition = (0, 0);
+        _FountainOfObjects = new Location(0, 2);
+        _PlayerLocation = new Location(0, 0);
     }
-
 }
 
-// Record to create Player class
-public record Player(char Symbol);
+
+public record Location(int row, int col);
 
 
-// Main Game Loop
-public class MainLoop
-{
-    public static void Start()
-    {
-        bool GameOver = false;
-        Grid MyGame = new Grid(4, 4);
-        MyGame.AddValuesToGrid();
-        State state = new State(MyGame);
-
-        while (!GameOver)
-        {
-            MyGame.Display();
-            state.CheckMovement();
-
-            string? input = Console.ReadLine();
-
-            Console.Clear();
-        }
-
-
-    }
-
-}
+public enum Rooms { Fountain, Player }
