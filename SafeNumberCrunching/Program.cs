115061fa4009a25c30ae8f6be04943e18231f79f
Hernan Di Tano <hditano@gmail.com>
2023-04-15 22:12:25 +0200

    Methods Revisted Exercise 1 Done

---
+++ SafeNumberCrunching/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Please type in a number: ");
+string value = Console.ReadLine();
+
+int.TryParse(value, out int result);
+Console.WriteLine(result);
+
+
