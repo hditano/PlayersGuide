28c64628060caaa550818468464711e715cab02e
Hernan Di Tano <hditano@gmail.com>
2023-04-18 05:56:49 +0200

    Event Exercise WIP

---
+++ CharberryTrees/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+CharberryTree tree = new CharberryTree();
+Notifier not = new Notifier(tree);
+
+while (true)
+    tree.MaybeGrow();
+
+
+public class CharberryTree
+{
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+    public event Action? Ripped;
+
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripped?.Invoke();
+        }
+    }
+}
+
+public class Notifier
+{
+    public Notifier(CharberryTree tree)
+    {
+        tree.Ripped += OnRippened;
+    }
+
+    private void OnRippened()
+    {
+        Console.WriteLine("Charberry fruit has been rippened");
+    }
+}
