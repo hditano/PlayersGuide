d1c44a8d176f4ec0e996f0bc893589b2fe7d4d8a
Hernan Di Tano <hditano@gmail.com>
2023-04-19 18:50:28 +0200

    Thread Exercise WIP

--- TheRepeatingStream/Program.cs
+++ TheRepeatingStream/Program.cs
@@ -4,27 +4,53 @@ Thread thread = new Thread(recent.Run);
 
 thread.Start();
 
-recent.Run();
 
 thread.Join();
 
-Console.WriteLine($"Main Thread: {recent._number1} {recent._number2}");
+Console.WriteLine($"Main Thread: {recent.Number1} {recent.Number2}");
 
 public class RecentNumbers
 {
     private object _numberGenerated = new object();
-    public Random rand = new Random();
+    public Random _rand;
     public int _number1;
     public int _number2;
+    
+    public RecentNumbers()
+    {
+        _rand = new Random();
+    }
+
+    public int Number1
+    {
+        get
+        {
+            lock (_numberGenerated)
+            {
+                return _number1;
+            }
+        }
+    }
+
+    public int Number2
+    {
+        get
+        {
+            lock(_numberGenerated)
+            {
+                return _number2;
+            }
+        }
+    }
+
 
     public void Run()
     {
-        _number1 = rand.Next(10);
-        Console.WriteLine($"Number 1: {_number1}");
-        _numberGenerated = _number1;
+        _number1 = _rand.Next(10);
+        Console.WriteLine($"Number 1: {_number1} Number 2: {_number2}");
         Thread.Sleep(2000);
-        _number2 = rand.Next(10);
-        Console.WriteLine($"Number 2: {_number2}");
+        _number2 = _rand.Next(10);
+        Console.WriteLine($"Number 2: {_number2} Number 1: {_number1}");
         Thread.Sleep(2000);
     }
 }
