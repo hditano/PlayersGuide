e921704d7bc6a7b8111c27ed4507b85c328ae194
Hernan Di Tano <hditano@gmail.com>
2023-04-19 19:31:23 +0200

    Thread Exercise 1 - WIP

---
+++ AsynchronousRandomWords/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(RandomlyRecreate("he"));
+
+int RandomlyRecreate(string word)
+{
+    int result = 0;
+    Random rand = new Random();
+    string tmpWord;
+
+    do
+    {
+        tmpWord = "";
+        result++;
+        for (int index = 0; index < word.Length; index++)
+        {
+            tmpWord += (char)('a' + rand.Next(26));
+            Console.WriteLine(tmpWord);
+        }
+    } while (tmpWord != word);
+
+    return result;
+    
+}
